fd9a04ddbd299108ab11984843fedfc0fb1d360f
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-06-12 09:45:56 +0700

    Update example to demonstrate dark mode, custom gradient, and custom shapes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer_alternative/shimmer_alternative.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Shimmer Alternative Example'),
+          title: const Text('Shimmer Alternative Example'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               ShimmerAlternative(
-                duration: Duration(seconds: 2),
+                duration: const Duration(seconds: 2),
                 direction: ShimmerDirection.ttb,
+                isDarkMode: true,
                 child: Container(
                   width: double.infinity,
                   height: 150.0,
                   color: Colors.grey[300],
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ShimmerAlternative(
-                duration: Duration(seconds: 3),
+                duration: const Duration(seconds: 3),
                 direction: ShimmerDirection.btt,
                 child: Text(
                   'Loading text...',
@@ -37,17 +43,46 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ShimmerAlternative(
-                duration: Duration(seconds: 1),
+                duration: const Duration(seconds: 1),
                 direction: ShimmerDirection.rtl,
                 child: TextField(
                   decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                    border: const OutlineInputBorder(),
                     labelText: 'Loading...',
                     fillColor: Colors.grey[300],
                     filled: true,
                   ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ShimmerAlternative(
+                customGradient: const LinearGradient(
+                  colors: [Colors.red, Colors.blue, Colors.green],
+                  stops: [0.4, 0.5, 0.6],
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: 150.0,
+                  color: Colors.grey[300],
+                ),
+              ),
+              const SizedBox(height: 20),
+              ShimmerAlternative(
+                shape: ShimmerShape.custom,
+                customShapeBuilder: (canvas, size, paint) {
+                  Path path = Path();
+                  path.moveTo(size.width * 0.5, 0);
+                  path.lineTo(size.width, size.height);
+                  path.lineTo(0, size.height);
+                  path.close();
+                  canvas.drawPath(path, paint);
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 150.0,
+                  color: Colors.grey[300],
                 ),
               ),
             ],
