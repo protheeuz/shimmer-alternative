523dbdf8fde57c35dbb7b89cc9e4b488cde311a1
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-06-13 02:06:35 +0700

    example: demonstrated animation control with pause and resume functionally

--- example/example.dart
+++ example/example.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer_alternative/shimmer_alternative.dart';
 
-/// Entry point of the application.
+/// Titik masuk dari aplikasi.
 void main() => runApp(const MyApp());
 
-/// The main application widget.
+/// Widget aplikasi utama.
 class MyApp extends StatelessWidget {
-  /// Creates a new instance of [MyApp].
+  /// Membuat instance baru dari [MyApp].
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -22,74 +22,94 @@ class MyApp extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                ShimmerAlternative(
-                  duration: const Duration(seconds: 2),
-                  direction: ShimmerDirection.ttb,
-                  isDarkMode: true,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150.0,
-                    color: Colors.grey[300],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ShimmerAlternative(
-                  duration: const Duration(seconds: 3),
-                  direction: ShimmerDirection.btt,
-                  child: Text(
-                    'Loading text...',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[300],
+            child: Builder(
+              builder: (BuildContext context) {
+                final GlobalKey<ShimmerAlternativeState> shimmerKey =
+                    GlobalKey<ShimmerAlternativeState>();
+                return Column(
+                  children: <Widget>[
+                    ShimmerAlternative(
+                      key: shimmerKey,
+                      duration: const Duration(seconds: 2),
+                      direction: ShimmerDirection.ttb,
+                      isDarkMode: true,
+                      child: Container(
+                        width: double.infinity,
+                        height: 150.0,
+                        color: Colors.grey[300],
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ShimmerAlternative(
-                  duration: const Duration(seconds: 1),
-                  direction: ShimmerDirection.rtl,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      labelText: 'Loading...',
-                      fillColor: Colors.grey[300],
-                      filled: true,
+                    ElevatedButton(
+                      onPressed: () {
+                        shimmerKey.currentState?.pauseAnimation();
+                      },
+                      child: const Text('Pause Animation'),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ShimmerAlternative(
-                  customGradient: const LinearGradient(
-                    colors: <Color>[Colors.red, Colors.blue, Colors.green],
-                    stops: <double>[0.4, 0.5, 0.6],
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    height: 150.0,
-                    color: Colors.grey[300],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ShimmerAlternative(
-                  shape: ShimmerShape.custom,
-                  customShapeBuilder: (Canvas canvas, Size size, Paint paint) {
-                    final Path path = Path();
-                    path.moveTo(size.width * 0.5, 0);
-                    path.lineTo(size.width, size.height);
-                    path.lineTo(0, size.height);
-                    path.close();
-                    canvas.drawPath(path, paint);
-                  },
-                  child: Container(
-                    width: double.infinity,
-                    height: 150.0,
-                    color: Colors.grey[300],
-                  ),
-                ),
-              ],
+                    ElevatedButton(
+                      onPressed: () {
+                        shimmerKey.currentState?.resumeAnimation();
+                      },
+                      child: const Text('Resume Animation'),
+                    ),
+                    const SizedBox(height: 20),
+                    ShimmerAlternative(
+                      duration: const Duration(seconds: 3),
+                      direction: ShimmerDirection.btt,
+                      child: Text(
+                        'Loading text...',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ShimmerAlternative(
+                      duration: const Duration(seconds: 1),
+                      direction: ShimmerDirection.rtl,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          labelText: 'Loading...',
+                          fillColor: Colors.grey[300],
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ShimmerAlternative(
+                      customGradient: const LinearGradient(
+                        colors: <Color>[Colors.red, Colors.blue, Colors.green],
+                        stops: <double>[0.4, 0.5, 0.6],
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150.0,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ShimmerAlternative(
+                      shape: ShimmerShape.custom,
+                      customShapeBuilder:
+                          (Canvas canvas, Size size, Paint paint) {
+                        final Path path = Path();
+                        path.moveTo(size.width * 0.5, 0);
+                        path.lineTo(size.width, size.height);
+                        path.lineTo(0, size.height);
+                        path.close();
+                        canvas.drawPath(path, paint);
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 150.0,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
